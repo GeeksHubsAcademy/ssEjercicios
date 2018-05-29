f7afddf7b248e6a1e016cbeba3bf660defd45320
Israel Salinas <israel.salinas.m@gmail.com>
2018-05-28 21:33:42 -0500

    firebase connection

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-function loadViews(selectedMenu){
+var config = {
+    apiKey: "AIzaSyAO-MtLjicwGW2uar6Y-wlbv3XOZyN0OC4",
+    authDomain: "fir-sample-dcd2c.firebaseapp.com",
+    databaseURL: "https://fir-sample-dcd2c.firebaseio.com",
+    projectId: "fir-sample-dcd2c",
+    storageBucket: "fir-sample-dcd2c.appspot.com",
+    messagingSenderId: "794168900181"
+};
+
+firebase.initializeApp(config);
+
+var customersList = firebase.database().ref('customers/');
+
+customersList.on('value', function(snapshot) {
+    console.log(snapshot.val())
+});
+
+function loadViews(selectedMenu) {
     var selectedView = $(selectedMenu).data("custom-ref")
-    $(".main-wrapper").load("./views/"+selectedView+".html")
+    $(".main-wrapper").load("./views/" + selectedView + ".html")
 }
+
+// Initialize Firebase
+
+
